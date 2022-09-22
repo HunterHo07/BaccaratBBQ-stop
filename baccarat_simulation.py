dd705f5457f67b9096cde6d74eab8e4f455f1f5a
Hunter <goldcool07@gmail.com>
2022-09-23 04:06:43 +0800

    master

--- baccarat_simulation.py
+++ baccarat_simulation.py
@@ -8,7 +8,7 @@ show1 = 0
 def create_shoe():
 	cards_set = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0] 	# 13-cards created	['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] 10, J, Q, K = 0 | A = 1
 	deck = cards_set * 4 									          # 4 set of cards 13cards each
-	table_deck = deck * 1 								          # 8 of decks for 1 table
+	table_deck = deck * 8 								          # 8 of decks for 1 table
 	random.shuffle(table_deck)						          # shuffle Deck 1
 	random.shuffle(table_deck)						          # shuffle Deck 2-time to be sure
 	discard = table_deck[0]								          # discard process, draw 1st card of the deck
@@ -16,8 +16,8 @@ def create_shoe():
 	table_deck_ready = table_deck[discard+1:]				# discard process, if 1st card is face or ten then discards 10 + 1, Else follow the number + 1
 
  # insert RED cards = cut cards to stop the deck from casino
-	# red = random.randint(270,300)			              # Get a random cut card position by random (208 = 50%) (260-290 = 30% left)
-	red = random.randint(27,30)			              # Get a random cut card position by random (208 = 50%) (260-290 = 30% left)
+	red = random.randint(290,320)			              # Get a random cut card position by random (208 = 50%) (300-320 = 25% left)
+	# red = random.randint(27,30)			              # Get a random cut card position by random (208 = 50%) (260-290 = 30% left) Testing use
 	table_deck_ready.insert(red,"STOP")             # Insert RED-CARD for Last round game of Baccarat cut-cards.
   
 
@@ -41,10 +41,12 @@ def play_game():										        # Game start play
   Win_results_past = ""                   # record which side winning, P, B, T from the past 1 game compare with past and now
   Table_deck = Start_Play_Deck						# Mirror the ready deck for this table (So we can always check back the original deck)
   Red_Card = False
+  Game_Count = 0                          # game counter
 
   #repeat the deck till RED-Card draw and stop
   while Red_Card == False:
       # print(Table_deck)									    # check original deck before play
+      Game_Count+=1
       
       #draw hand from the decks
       for i in range(4):
@@ -64,28 +66,60 @@ def play_game():										        # Game start play
       #check player & banker hand total value
       player_hand = (player1 + player2) % 10				#Process player hands & -10 if the both cards add-up more than 10
       banker_hand = (banker1 + banker2) % 10				#Process banker hands & -10 if the both cards add-up more than 10
+      player3 = ''
+      banker3 = ''
+      # print(Table_deck)									#check Decks after drew cards
+
+# banker 9,8 = Done | 7
+# Player 9,8 = Done
+      if (player_hand < 8 and banker_hand < 8):			#if no natural win then check for 3rd card draw for player or banker
+        #check player or banker need to draw 3rd card
+          if player_hand <= 5:                           #player total 0,1,2,3,4 5, then player draw 3rd card
+            check_card = Table_deck.pop(0)
+            if check_card == "STOP":                          #check if the red card is draw
+              Red_Card = True
+              print("STOP")
+              check_card = Table_deck.pop(0)
+            player3 = check_card							                #player draw 3rd card
+            player_hand = (player1 + player2 + player3) % 10	#Process player hands & -10 if the both cards add-up more than 10
+
+            #check banker 3rd card draw condition
+            if(banker_hand <= 2) or (banker_hand == 3 and player3 != 8) or (banker_hand == 4 and player3 >= 2 and player3 <= 7) or (banker_hand == 5 and player3 >= 4 and player3 <= 7) or (banker_hand == 6 and (player3 == 6 or player3 == 7)):
+              check_card = Table_deck.pop(0)
+              if check_card == "STOP":                        #check if the red card is draw
+                Red_Card = True
+                print("STOP")
+                check_card = Table_deck.pop(0)
+              banker3 = check_card							              #banker draw 3rd card
+              banker_hand = (banker1 + banker2 + banker3) % 10#Process banker hands & -10 if the both cards add-up more than 10
+
+          elif banker_hand <= 5 and (player_hand == 6 or player_hand == 7):
+            check_card = Table_deck.pop(0)
+            if check_card == "STOP":                        #check if the red card is draw
+              Red_Card = True
+              print("STOP")
+              check_card = Table_deck.pop(0)
+            banker3 = check_card							              #banker draw 3rd card
+            banker_hand = (banker1 + banker2 + banker3) % 10#Process banker hands & -10 if the both cards add-up more than 10
+
       
-      print('Player_Hand:', player1, player2, '=', player_hand)					#check player face-up cards
-      print('banker_Hand:', banker1, banker2, '=', banker_hand)					#check banker face-up cards
+      print('Player_Hand:', player1, player2, player3,'=', player_hand)					#check player face-up cards
+      print('Banker_Hand:', banker1, banker2, banker3,'=', banker_hand)					#check banker face-up cards
       # print(Table_deck)									#check Decks after drew cards
 
       
-      # player_hand = 8
-      # banker_hand = 6
-      if (player_hand < 8 and banker_hand < 8):			#if no natural win then check for 3rd card draw for player or banker
-        #check player or banker need to draw 3rd card
-          if ((player_hand == 6 or player_hand == 7) and player_hand == banker_hand):                    #The Player stands on totals of 6 or 7 and check if player & banker tie on 6 or 7
-            pass
-
-      
+      #check player or banker win or Tie
       if (player_hand > banker_hand):
         Win_results = "P"
+        print("P-WIN")
       elif (player_hand < banker_hand):
         Win_results = "B"
+        print("BB-WIN")
       else:
         Win_results = "T"
+        print("TIE")
 
-
+      #check if winning in row
       if Win_results_past == Win_results: 
         Win_row += 1
       elif Win_results_past == '':
@@ -97,8 +131,8 @@ def play_game():										        # Game start play
         Game_results = Game_results + [Win_results+str(Win_row)]
 
 
-      print(Win_results_past ,Win_results)
+      # print(Win_results_past ,Win_results)            # Check each winning row
       Win_results_past = Win_results
-      print(Game_results)
+      print(Game_Count, Game_results)
       
 play_game()
