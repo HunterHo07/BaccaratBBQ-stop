2d4cca45f28b4d4da10dda408c37fb52a0f3dafd
Hunter <goldcool07@gmail.com>
2022-09-25 06:42:20 +0800

    master

--- baccarat_simulation.py
+++ baccarat_simulation.py
@@ -34,13 +34,15 @@ def create_shoe():
 # Play the game
 def play_game():										        # Game start play
   Start_Play_Deck = create_shoe()							# Take the ready deck to action and start to play the game of baccarat
-  Game_results = []                         # record game results
-  Win_row = 1                               # record same winning side count / Start with 1
+  Game_results= []                         # record game results
+  Win_row     = 1                               # record same winning side count / Start with 1
   Win_results = ""                        # record which side winning, P, B, T
   Win_results_past = ""                   # record which side winning, P, B, T from the past 1 game compare with past and now
-  Table_deck = Start_Play_Deck						# Mirror the ready deck for this table (So we can always check back the original deck)
-  Red_Card = False
-  Game_Count = 0                          # game counter
+  Table_deck  = Start_Play_Deck						# Mirror the ready deck for this table (So we can always check back the original deck)
+  Red_Card    = False
+  Game_Count  = 0                          # game counter
+  win_p       = 0
+  win_b       = 0
 
   #repeat the deck till RED-Card draw and stop
   while Red_Card == False:
@@ -107,9 +109,11 @@ def play_game():										        # Game start play
       #check player or banker win or Tie
       if (player_hand > banker_hand):
         Win_results = "P"
+        win_p+=1
         # print("P-WIN")
       elif (player_hand < banker_hand):
         Win_results = "B"
+        win_b+=1
         # print("BB-WIN")
       else:
         Win_results = "T"
@@ -130,58 +134,30 @@ def play_game():										        # Game start play
       # print(Win_results_past ,Win_results)            # Check each winning row
       Win_results_past = Win_results
       # print(Game_Count, Game_results)
-  return(Game_Count, Game_results)
-
-def test_case():
-  results = play_game()       # Take the results from random game
-  results = results[1]        # take out the number of the game
-  results_test1=results
-
-  # Clean up the test results
-  while results_test1[0][0] == "P" or results_test1[0][0] == "T":  # pop 1st T or P
-    results_test1 = results_test1[1:]
-  while results_test1[2][0] == "P" or results_test1[2][0] == "T":
-    results_test1.pop(2)
-
-  # print(results)
-  # print(results_test1)
-  return(results_test1)
-      
-
-# Step 3 play many tables simulation
-
-skip  = 0                   # count skip condition meet
-win   = 0
-lose  = 0
-for i in range(1,10001):  #10k results about 4.5sec
-  # print(i,test_case())
-  # print("Test")
-  # play_game()
-
-  #test case for testing simulation (skip-4 1-B3 or 2-B3)
-  test_1 = test_case()
-  #test case after skip & bet
-  # > 0 = -2910 vs 4236 = 1326  Win =  2118  | Lose = 970   | 0.1326
-  # > 1 = -1470 vs 2086 = 616
-  # > 2 = -1065 vs 1358 = 293
-  # > 3 = -726 vs 1054  = 328
-  if skip > 0:
-    # print("start")
-    if int(test_1[0][1]) > 2 or int(test_1[2][1]) > 2:
-      lose += 1
-    else:
-      win += 1
-    skip = 0
-    # print("Win = ",win, " | Lose =", lose)
-
-
-  if int(test_1[0][1]) > 2 or int(test_1[2][1]) > 2:
-    skip+=1
-    # print("Skip", skip)
+  return(Game_Count, Game_results, win_p, win_b)
 
 
 
-  # print(i,  "=" ,test_1)
-print("Win = ",win, " | Lose =", lose)
+# Step 3 Test check simulation results is correct of 49 vs 51
+def test_1():
+  skip  = 0                   # count skip condition meet
+  Player= 0
+  Banker= 0
+  for i in range(1,10001):  #10k results about 4.5sec
+    results = play_game()       # Take the results from random game
+    results_p = results[2]        # take out the number of the game
+    results_b = results[3]        # take out the number of the game
+
+    Player +=results_p
+    Banker +=results_b
+
+  Total = Player + Banker
+  Player_per = (Player / Total) * 100
+  Banker_per = (Banker / Total) * 100
+    
+  print("Player:",Player , round(Player_per,2) ,"| Banker:", Banker, round(Banker_per,2),  " | Total:",Total)
+
+test_1()
+
 
 
