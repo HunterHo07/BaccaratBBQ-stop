532f7a156fb3dda38b7d8c3d60f3726afe074b46
Hunter <goldcool07@gmail.com>
2022-09-28 03:40:06 +0800

    master

--- baccarat_real_results.py
+++ baccarat_real_results.py
@@ -11,7 +11,7 @@ with open('real_results/bac1.txt') as f:
 
 results_holder = []
 #get the 25,000 results & clean it
-for i in range(10):
+for i in range(25000):
     results_holder = results_holder + [lines[i]]
 
 # remove all Tie from the results
@@ -51,13 +51,146 @@ for i in results_holder:
             check_results = check_results + [past+str(win_count)]
             win_count=1
         past = now
+    if check_results[0][0] == "P":
+        # print(check_results, "Before")
+        check_results = check_results[1:]
+        # print(check_results, "After")
     all_check_results.append(check_results)
     check_results=[]
     # print(i)
-    
+
 #clean up all results
-print(all_check_results)
+# print(all_check_results)
 
 
+
+
+
+
+
+
+
+# Step 5 play many tables simulation
+def test_7():
+  W1_win   = 0
+  L1_lose  = 0
+  W2_win   = 0
+  L2_lose  = 0
+  W3_win   = 0
+  L3_lose  = 0
+  skip     = 0 
+  skip1    = 0 
+  skip_win = 0 
+  skip1_win= 0
+  for test_1 in all_check_results:  #10k results about 4.5sec
+    # test_1 = test_case()       # Take the results from random game
+    # print(test_1)
+
+    # Bet all & Check
+    #Bet-1
+    if int(test_1[0][1]) > 1:  #if 1st or 2nd B- is more than 3 win in the row
+      L1_lose+=1
+    else:
+      W1_win+=1
+
+      #Bet-2
+      if int(test_1[2][1]) > 2:  #if 1st or 2nd B- is more than 3 win in the row
+        L1_lose+=3
+      else:
+        W1_win+=1
+
+        #Bet-3
+        if int(test_1[4][1]) > 2:  #if 1st or 2nd B- is more than 3 win in the row
+          L1_lose+=3
+        else:
+          W1_win+=1
+
+
+    #Bet-1
+    if skip > 2:
+      #Bet-1
+      if int(test_1[0][1]) > 1:  #if 1st or 2nd B- is more than 3 win in the row
+        L2_lose+=1
+        skip=0
+      else:
+        W2_win+=1
+        skip_win+=1
+
+        #Bet-2
+        if int(test_1[2][1]) > 2:  #if 1st or 2nd B- is more than 3 win in the row
+          L2_lose+=1
+          skip=0
+        else:
+          W2_win+=1
+          skip_win+=1
+
+          #Bet-3
+          if int(test_1[4][1]) > 2:  #if 1st or 2nd B- is more than 3 win in the row
+            L2_lose+=3
+            skip=0
+          else:
+            W2_win+=1
+            skip_win+=1
+
+    if int(test_1[0][1]) > 2 and int(test_1[2][1]) > 2:
+      skip+=1
+    if skip_win > 8:
+      skip_win=0
+      skip=0
+
+
+    #Bet-1
+    if skip1 > 2:
+      #Bet-1
+      if int(test_1[0][1]) > 1:  #if 1st or 2nd B- is more than 3 win in the row
+        L3_lose+=1
+        skip1=0
+      else:
+        W3_win+=1
+        skip1_win+=1
+
+        #Bet-2
+        if int(test_1[2][1]) > 2:  #if 1st or 2nd B- is more than 3 win in the row
+          L3_lose+=1
+          skip1=0
+        else:
+          W3_win+=1
+          skip1_win+=1
+
+          #Bet-3
+          if int(test_1[4][1]) > 2:  #if 1st or 2nd B- is more than 3 win in the row
+            L3_lose+=3
+            skip1=0
+          else:
+            W3_win+=1
+            skip1_win+=1
+
+    if int(test_1[0][1]) > 2 and int(test_1[2][1]) > 2:
+      skip1+=1
+    if skip1_win > 7:
+      skip1_win=0
+      skip1=0
+
+
+
+    # print(i,  "=" ,test_1)
+    # print("Win-1:", round(W_win,2) , " | Win-2:", round(L_lose,2) , " || Total:", round(total,2))
+  total3 = W3_win - L3_lose
+  total_3 = W3_win + L3_lose
+  total_per3 = (W3_win / total_3) * 100 #2.3 - 5.51
+  total2 = W2_win - L2_lose
+  total_2 = W2_win + L2_lose
+  total_per2 = (W2_win / total_2) * 100 #2.3 - 5.51
+  total1 = W1_win - L1_lose
+  total_1 = W1_win + L1_lose
+  total_per1 = (W1_win / total_1) * 100 #2.3 - 5.51
+  print("1-0 Win-1:", round(W1_win,2) , " | Lose-1:", round(L1_lose,2) , " || Total-1:", round(total1,2) , "|", round(total_per1,2))
+  print("1-1 Win-2:", round(W2_win,2) , " | Lose-2:", round(L2_lose,2) , " || Total-2:", round(total2,2) , "|", round(total_per2,2))
+  print("1-2 Win-3:", round(W3_win,2) , " | Lose-3:", round(L3_lose,2) , " || Total-3:", round(total3,2) , "|", round(total_per3,2))
+
+# 1-0 Win-1: 21651  | Lose-1: 28161  || Total-1: -6510 | 43.47
+# 1-1 Win-2: 763  | Lose-2: 884  || Total-2: -121 | 46.33
+# 1-2 Win-3: 761  | Lose-3: 874  || Total-3: -113 | 46.54
+test_7()
     
 
